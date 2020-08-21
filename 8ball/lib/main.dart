d8685f908b9e50e7954d2eaaa8a1edbca16bcdea
Govind B Chandran <govindchandran235@gmail.com>
2020-08-21 22:26:28 +0530

    Add files via upload

---
+++ 8ball/lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Center(
+            child: Text('Magic 8 Ball')),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
